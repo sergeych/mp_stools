ab239f12f3ceaf3e2e386290d6da459b9bc752f2
sergeych <real.sergeych@gmail.com>
2022-04-19 00:42:45 +0200

    stable 1.2.2. improved globalLaunch to silently accept cancellation and globalDefer, together removes the problem of silly GlobalScope warnings.

--- src/commonMain/kotlin/net/sergeych/mp_tools/async_tools.kt
+++ src/commonMain/kotlin/net/sergeych/mp_tools/async_tools.kt
@@ -8,12 +8,27 @@ import net.sergeych.mp_logger.exception
 
 private val log = LogTag("MPTLS")
 
+
+/**
+ * Launch a standalone cancellable coroutine using GlobalScope. Eats warnings (where possible) and
+ * do not report `CancellationException`. Other unhandled exceptions are logged with usual [Log] means.
+ */
 fun globalLaunch(block: suspend CoroutineScope.()->Unit): Job =
     GlobalScope.launch {
         try {
             block()
         }
+        catch(_: CancellationException) {
+            // this is OK for global launch
+        }
         catch(t: Throwable) {
             log.exception { "unexpected in globalLaunch" to t }
         }
     }
+
+
+/**
+ * Calculate block in standalone coroutine and return its deferred result
+ */
+@Suppress("unused")
+fun <T>globalDefer(block: suspend CoroutineScope.() -> T): Deferred<T> = GlobalScope.async { block() }
